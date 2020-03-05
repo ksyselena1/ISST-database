cd642864722d8093f71de313bc03bf2996ec19a1
Selena Kang (Seo Young) <sk2426@cornell.edu>
2020-03-05 03:18:21 -0500

    complete m2

--- documents/design-journey.md
+++ documents/design-journey.md
@@ -32,6 +32,25 @@ These catalogs use broader categories to organize each type and branch into smal
 > Label all images. All labels must be visible in VS Code's Markdown Preview.
 > Clearly label the final design.
 
+First sketch:
+
+![](first.jpg)
+
+This is the first sketch I created for the course catalog. I put all the content I would need on the website and there is no color. It is a basic ordering of what I wanted on the website.
+I am not satisfied with the layout or the aesthetics of the website.
+
+
+Second sketch:
+
+![](second.jpg)
+
+This is the second draft of the website design for the course catalog. I changed the position of the search bar and made the form to be vertically aligned to the left side with the add button on the right side to follow users' eyes. It has all the content needed, has a clear layout but I think it is not very pleasing yet.
+
+Final design:
+
+![](coursecatalogdesign.jpg)
+
+This was my final design. I like that it has a simple layout with clear headers so that users can focus effectively on the content. It is usable and is easy to use so users can search, add, and observe courses, which was the purpose of the website. I am also satisfied with the layout and the color of the design.
 
 ## Partials (Milestone 2)
 > If you have any partials, plan them here.
@@ -42,12 +61,11 @@ This catalog will be on one page, so partials are not necessary.
 > Describe the structure of your database. You may use words or a picture. A bulleted list is probably the simplest way to do this. Make sure you include constraints for each field.
 
 Table: courses
-- id: INTEGER {PK, U, NN, AI}
+- id: INTEGER {PK, U, NN}
 - name: TEXT {NN}
 - numcredits: INTEGER {NN}
 - req: TEXT {NN}
 - description: TEXT {NN}
-- professorname: TEXT
 
 
 ## Database Query Plan (Milestone 2)
@@ -63,13 +81,13 @@ Table: courses
 2. Search records
 
     ```
-    from courses get user input find records
+    from one of the field chosen in the courses data table, get user input find records that match the user input
     ```
 
 3. Insert record
 
     ```
-    from courses insert user input into db
+    get user input, insert user input into database matching its corresponding fields
     ```
 
 
--- index.php
+++ index.php
@@ -1,4 +1,46 @@
-<?php include("includes/init.php"); ?>
+<?php include("includes/init.php");
+$title = "Course Catalog";
+
+$db = open_sqlite_db('secure/catalog.sqlite');
+
+
+function printresult($entry)
+{
+?>
+  <tr>
+    <td><?php echo htmlspecialchars($entry["id"]); ?></td>
+    <td><?php echo htmlspecialchars($entry["name"]); ?></td>
+    <td><?php echo htmlspecialchars($entry["numcredits"]); ?></td>
+    <td><?php echo htmlspecialchars($entry["req"]); ?></td>
+    <td><?php echo htmlspecialchars($entry["description"]); ?></td>
+
+  </tr>
+<?php
+}
+$messages = array();
+const SEARCH_F = [
+  "id" => "By Course ID",
+  "name" => "By Course Name",
+  "req" => "By Requirement Fulfilled"
+];
+if (isset($_GET['search']) and isset($_GET['field'])) {
+  $search = TRUE;
+  $field = filter_input(INPUT_GET, 'field', FILTER_SANITIZE_STRING);
+  if (in_array($field, array_keys(SEARCH_F))) {
+    $search_f = $field;
+  } else {
+    array_push($messages, "You cannot search with this field.");
+    $search = FALSE;
+  }
+  $searched = filter_input(INPUT_GET, 'searched', FILTER_SANITIZE_STRING);
+  $searched = trim($searched);
+} else {
+  // No search provided, so set the product to query to NULL
+  $search = FALSE;
+  $field = NULL;
+  $searched = NULL;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,13 +57,114 @@
 </header>
 
 <body>
+  <main>
+    <h2><?php echo $title; ?></h2>
+    <p>Welcome to the course catalog for ISST (Information, Science, Systems, and Technology) majors.</p>
 
-  <!-- TODO: This should be your main page for your site. Remove this file when you're ready!-->
+    <?php
+    foreach ($messages as $message) {
+      echo "<p><strong>" . htmlspecialchars($message) . "</strong></p>\n";
+    }
+    ?>
+
+    <form id="searchForm" action="index.php" method="get">
+      <select name="field">
+        <option value="" selected disabled>Search By</option>
+        <?php
+        foreach (SEARCH_F as $field => $label) {
+        ?>
+          <option value="<?php echo $field; ?>"><?php echo $label; ?></option>
+        <?php
+        }
+        ?>
+      </select>
+      <input type="text" name="search" />
+      <button type="submit">Search</button>
+    </form>
+
+    <?php
+    if ($search) {
+    ?>
+      <h2>Search Results</h2>
+    <?php
+      $sql = "SELECT * FROM courses WHERE " . $search_field . " LIKE '%' || :searched || '%'";
+      $params = array(
+        ':searched' => $searched
+      );
+    } else {
+    ?>
+      <h2>All Courses</h2>
+    <?php
+
+      $sql = "SELECT* FROM courses;";
+      $params = array();
+    }
+
+    $result = exec_sql_query($db, $sql, $params);
+    $records = $result->fetchAll();
+    if (isset($records) and !empty($records)) {
+    ?>
+      <table>
+        <tr>
+          <th>Course ID</th>
+          <th>Course Name</th>
+          <th># of Credits</th>
+          <th>Fulfills Requirement</th>
+          <th>Description</th>
+        </tr>
+        <?php
+        foreach ($records as $entry) {
+          printresult($entry);
+        }
+
+        ?>
+      </table>
+    <?php
+    } else {
+      echo "<p>No courses match your search</p>";
+    }
+
+    ?>
+    </table>
+    <?php
+    ?>
+    <div>
+      <h2>Fill out this form to add a course to the catalog:</h2>
+      <form id="courseform" method="post" action="index.php" novalidate>
+        <div>
+          <label for="id_field">Course ID:</label>
+          <input id="id_field" type="text" name="form_id" required />
+        </div>
+        <div>
+          <label for="name_field">Course Name:</label>
+          <input type="text" id="name_field" name="form_name" required />
+        </div>
+        <div>
+          <label for="cred_field"># of Credits:</label>
+          <input type="number" id="cred_field" name="form_cred" min=1 max=5 required />
+        </div>
+        <div>
+          <label for="req_field">Requirement fulfilled:</label>
+          <input type="text" id="req_field" name="form_req" required />
+        </div>
+        <div>
+          <label for="description_field">Description:</label>
+          <input type="text" id="description_field" name="form_description" required />
+        </div>
+
+        <div class="button">
+          <span></span>
+          <input type="submit" value="Add Course" />
+        </div>
+
+      </form>
+    </div>
+  </main>
 
 </body>
 
 <footer>
-  <p>Go to <a href="https://infosci.cornell.edu/undergraduate/info-sci-majors/bs-information-science-systems-and-technology">Official Cornell website</a> for more information.</p>
+  Please visit the <a href="https://infosci.cornell.edu/undergraduate/info-sci-majors/bs-information-science-systems-and-technology/degree-requirements">Office Cornell website</a> for more information.
 </footer>
 
 </html>
